3ba518d2d5d083f7f2598352d0d03282bb8c4c00
mark <mark@192.168.0.108>
2012-01-20 10:15:22 +0100

    * 'Add text block' action - block is not persisted yet

---
+++ app/assets/javascripts/case.js
@@ -0,0 +1,40 @@
+
+
+$(function() {
+    $('#link-create-html-block').click(function(e) {              
+       var block = $('<div/>').addClass('block text');
+       var title = $('<h3/>').addClass('block-title').text('Text block');       
+       var f = $('<form/>');
+       
+       var textareaId = 'textarea-' + new Date().valueOf();
+       var textarea = $('<textarea>').attr('id', textareaId);
+       f.append(textarea);
+       
+       var actions = $('<div/>').addClass('actions');
+       var save = $('<input/>').attr('type', 'submit').val('Save block');           
+       var cancel = $('<a/>').addClass('cancel-block')
+                             .attr('href', '#')
+                             .text('Cancel');
+       actions.append(save).append(cancel);
+       f.append(actions);       
+       block.append(title).append(f);
+       $('.blocks-area').append(block);
+       
+       tinyMCE.execCommand('mceAddControl', false, textareaId);
+       
+       return false;
+    });
+    
+    $('.blocks-area .block a.cancel-block').live('click', function(e) {        
+        $(this).parents('.block:first').remove();        
+        return false;
+    });
+    
+    $('.blocks-area .block a.remove-block').live('click', function(e) {
+        if (confirm('Are you sure you want to remove this block?')) {
+            $(this).parents('.block:first').remove();
+        }
+        return false;
+    });
+});
+
