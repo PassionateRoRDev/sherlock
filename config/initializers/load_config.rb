4ebae41a672fd115ddcaec6d04b265f35e992e06
Johnathon E. Wright <jw@mustmodify.com>
2012-02-02 17:07:27 -0600

    add 'application' namespace to config file.

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,1 +1,7 @@
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+config = YAML.load_file("#{Rails.root}/config/config.yml")
+
+app_config = config['application']
+env_config = config[Rails.env]
+
+APP_CONFIG = app_config.merge(env_config)
+
