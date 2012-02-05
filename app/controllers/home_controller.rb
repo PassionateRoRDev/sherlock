cac0de57d6bef3df5e1f6e0d4336205922603634
ambisoft <mark@ambisoft.pl>
2012-02-05 16:31:02 +0100

    Incorporating the design/css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,8 @@ class HomeController < ApplicationController
   
   before_filter :authenticate_user!
   
-  def index
-    
-    @cases = current_user.cases
-    
+  def index    
+    @cases = current_user.cases    
   end
-
+  
 end
