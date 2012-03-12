d9d4dac0c7f3cd3a898b56ca6e60378b1b86ba17
ambisoft <mark@ambisoft.pl>
2012-03-12 22:01:55 +0100

    Mailer configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,8 @@ Sherlock::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  
+  config.action_mailer.default_url_options = { :host => 'www.sherlockdocs.com' }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
