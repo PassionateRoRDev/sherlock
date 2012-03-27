e3a9ac96a7f3bfeaacf752a86ea1f1e968546a3b
ambisoft <mark@ambisoft.pl>
2012-03-27 18:48:58 +0200

    Including footer in the HTML preview

--- app/helpers/footers_helper.rb
+++ app/helpers/footers_helper.rb
@@ -4,5 +4,11 @@ module FootersHelper
     footer = kase.footer    
     footer ? edit_case_footer_path(kase, footer) : new_case_footer_path(kase)          
   end
-
+  
+  def footer_style(footer)  
+    result = "text-align:#{footer.text_align};"
+    result += "font-size:#{footer.font_size}px;"
+    result += "font-weight:bold;"
+    result += "color:#{footer.font_color};"        
+  end
 end
