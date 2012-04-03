6df3cdb76daf40a781b3c5e022f4eacddeb35df7
ambisoft <mark@ambisoft.pl>
2012-04-03 22:54:21 +0200

    Case creation/deletion tracking

---
+++ app/models/case_observer.rb
@@ -0,0 +1,23 @@
+class CaseObserver < ActiveRecord::Observer
+
+  observe Case
+
+  def after_create(model)    
+    subject = model.class.to_s
+    Event.create( 
+      :event_type => 'create',
+      :event_subtype => 'case',
+      :detail_i1 => model.id 
+    )         
+  end
+  
+  def after_destroy(model)
+    subject = model.class.to_s
+    Event.create( 
+      :event_type => 'delete',
+      :event_subtype => 'case',
+      :detail_i1 => model.id 
+    )
+  end  
+
+end
