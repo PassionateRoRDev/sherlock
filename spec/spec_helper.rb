ec52ef82ce011352dda2d76131585d6cf2de98f4
ambisoft <mark@ambisoft.pl>
2012-04-06 12:27:42 +0200

    Cleaning tmp files after each test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,14 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  config.after(:each) do
+    # clean up any tmp user files
+    pics_path = Rails.root.to_s + '/' + APP_CONFIG['files_path']
+    Dir[pics_path + '*'].each do |f|       
+      FileUtils.rm_rf(f)       
+    end
+  end
+  
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
