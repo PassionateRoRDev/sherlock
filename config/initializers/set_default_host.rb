32208905f400d5e4c1100d6ed5c35a46e0f1306f
ambisoft <mark@ambisoft.pl>
2012-04-12 08:46:26 +0000

    set_default_host not needed anymore

--- config/initializers/set_default_host.rb
+++ config/initializers/set_default_host.rb
@@ -1,11 +1,11 @@
-module DefaultHostHelper
-  def set_default_host
-    ActionMailer::Base.default_url_options[:host] = request.host_with_port
-  end
-
-  def self.included( parent )
-    parent.before_filter :set_default_host
-  end
-end
-Devise::PasswordsController.send(:include, DefaultHostHelper)
-
+#module DefaultHostHelper
+#  def set_default_host
+#    ActionMailer::Base.default_url_options[:host] = request.host_with_port
+#  end
+#
+#  def self.included( parent )
+#    parent.before_filter :set_default_host
+#  end
+#end
+#Devise::PasswordsController.send(:include, DefaultHostHelper)
+#
