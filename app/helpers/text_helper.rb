0efa052abbb84057046bee358ed173a26cfb088b
ambisoft <mark@ambisoft.pl>
2012-05-12 19:11:35 +0200

    More friendly date format used

--- app/helpers/text_helper.rb
+++ app/helpers/text_helper.rb
@@ -4,4 +4,12 @@ module TextHelper
     s.to_s.present? ? s : default
   end
   
+  def print_date_or(s, default = '-')    
+    s.to_s.present? ? print_formatted_date(s) : default    
+  end
+  
+  def print_formatted_date(s)
+    s.in_time_zone('Eastern Time (US & Canada)').to_formatted_s(:date_time)
+  end
+  
 end
--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -1,1 +1,3 @@
 Date::DATE_FORMATS[:default] = '%m/%d/%Y'
+
+Time::DATE_FORMATS[:date_time] = '%-m/%d/%Y %H:%M %Z'
