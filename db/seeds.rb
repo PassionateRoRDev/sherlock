7fb74fe54b49eba136e15b5a6b1aa30208a24512
ambisoft <mark@ambisoft.pl>
2012-05-23 08:16:32 +0200

    Updating plans in the seeds.rb, adding Company plan

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,29 +20,41 @@ plans = [
     :chargify_handle  => :independent,
     :chargify_slug    => 'h/285859',
     :price            => 9,        
-    :cases_max        => 2,
-    :extra_case_price => 5,
-    :clients_max      => 10,
+    :cases_max        => 3,
+    :extra_case_price => 4,
+    :clients_max      => 15,
     :storage_max_mb   => 1024 * 2
   },
   {
     :chargify_handle  => :agency,
     :chargify_slug    => 'h/286025',
     :price            => 29,
-    :cases_max        => 10,
+    :cases_max        => 12,
     :extra_case_price => 3,
     :clients_max      => 100,
     :storage_max_mb   => 1024 * 5
   },
+  
+  {
+    :chargify_handle  => :company,
+    :chargify_slug    => 'h/927622',
+    :price            => 59,    
+    :cases_max        => 30,
+    :extra_case_price => 2,
+    :clients_max      => 250,
+    :storage_max_mb   => 1024 * 12
+  },
+  
   {
     :chargify_handle  => :corporate,
     :chargify_slug    => 'h/286061',
     :price            => 99,    
-    :cases_max        => 50,
-    :extra_case_price => 2,
+    :cases_max        => 60,
+    :extra_case_price => 1,
     :clients_max      => 0,
     :storage_max_mb   => 1024 * 25
   }
+  
 ]
 
 plans.each { |plan| SubscriptionPlan.find_or_create_by_chargify_handle plan }
