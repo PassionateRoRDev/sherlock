46b3fac1c86767a2be469f32a9c18652c04aafc0
popperur <popperur@gmail.com>
2013-03-23 18:13:55 +0100

    Scheduler for subscription check is switched off for now, see comments inside

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,8 @@
 # More info about "whenever" scheduler:
 # https://github.com/javan/whenever/
 
-# set :environment, 'development' 
-
-every 1.minutes do
-  runner 'Subscription.revise_records'
-end
+# Currently it's added manually, because RVM has problems with whenever
+# TODO: See the description about RVM in the homepage, make the changes and then put these lines back 
+# every 12.hours do
+#   runner 'Subscription.revise_records'
+# end
